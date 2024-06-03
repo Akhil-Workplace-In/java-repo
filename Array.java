510abdb3cb51a331083d4c2fb2016eb314f51181
142288571+Akhil-Workplace-In <142288571+Akhil-Workplace-In@users.noreply.github.com>
2024-06-03 19:02:50 +0530

    Created two dimentional Array by using random Numbers

--- Array.java
+++ Array.java
@@ -3,23 +3,34 @@ class Array
    public static void main(String args[])
    {
       
-      int nums[][] = new int[10][10];
+      int nums[][] = new int[3][4];
            
 
       for(int i=0; i<3; i++)
       {
          for(int j=0; j<4; j++)
          {
-            nums[i][j] = (int) (Math.random() * 100); 
+            nums[i][j] = (int) (Math.random() * 10); 
          }
          
       }
 
-      for(int i=0; i<3; i++)
+      // for(int i=0; i<3; i++)
+      // {
+      //    for(int j=0; j<4; j++)
+      //    {
+      //       System.out.print(nums[i][j] + " ");
+      //    }
+      //    System.out.println();
+      // }
+
+      
+
+      for(int n[]: nums)
       {
-         for(int j=0; j<4; j++)
+         for(int m : n)
          {
-            System.out.print(nums[i][j] + " ");
+            System.out.print(m + " ");
          }
          System.out.println();
       }
